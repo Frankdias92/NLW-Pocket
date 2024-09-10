f2e2756ade6a6c641a7ec1cdcf667a93a015d6a3
Frankdias92 <frankmcdias@gmail.com>
2024-09-10 14:33:31 -0300

    :sparkles: Feat: first route create

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,15 +1,28 @@
-import { db } from '.'
+import { client, db } from '.'
 import { goalCompletions, goals } from './schema'
+import dayjs from 'dayjs'
 
 async function seed() {
   //   await db.delete(goalCompletions).execute()
   //   await db.delete(goals)
 
-  await db.insert(goals).values([
-    { title: 'Wake up early', desiredWeeklyFrequency: 5 },
-    { title: 'Workout', desiredWeeklyFrequency: 3 },
-    { title: 'Meditation', desiredWeeklyFrequency: 1 },
+  const result = await db
+    .insert(goals)
+    .values([
+      { title: 'Wake up early', desiredWeeklyFrequency: 5 },
+      { title: 'Workout', desiredWeeklyFrequency: 3 },
+      { title: 'Meditation', desiredWeeklyFrequency: 1 },
+    ])
+    .returning()
+
+  const startOfWeek = dayjs().startOf('week')
+
+  await db.insert(goalCompletions).values([
+    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
   ])
 }
 
-seed()
+seed().finally(() => {
+  client.end()
+})
