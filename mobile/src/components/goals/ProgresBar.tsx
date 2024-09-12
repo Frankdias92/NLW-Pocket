07657558d4ebcec7aa99ee628728ab96926bc5dd
Frankdias92 <frankmcdias@gmail.com>
2024-09-12 17:59:21 -0300

    :bug: Fix: working on progress bar

--- mobile/src/components/goals/ProgresBar.tsx
+++ mobile/src/components/goals/ProgresBar.tsx
@@ -1,11 +1,22 @@
 import { Center, HStack, Text } from "@gluestack-ui/themed";
-import { View } from "react-native";
+import { ProgressBarAndroid, View, StyleSheet } from "react-native";
 
 
 export function ProgresBar() {
     return (
         <Center w={"$full"} gap={12} px={'$8'}>
-            <View className="min-w-full h-2 bg-violet-600 rounded-full"/>
+            <View 
+                className="min-w-full h-2 bg-violet-600 rounded-full"
+                style={styles.example}
+            >
+                <ProgressBarAndroid
+                styleAttr="Horizontal"
+                indeterminate={false}
+                progress={0.75}
+                />
+            </View>
+            
+            {/* <View className="min-w-full h-2 bg-violet-600 rounded-full"/> */}
             <HStack w={"$full"} justifyContent="space-between">
                 <Text alignSelf="flex-start" color="$secondary400">
                     Você completou <Text color="$secondary100">8</Text> de <Text color="$secondary100">15</Text>.
@@ -18,3 +29,15 @@ export function ProgresBar() {
         </Center>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    example: {
+      marginVertical: 24,
+    },
+  });
