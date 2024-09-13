f7bdb7a7cc029c611b9da986ababf1b60b0babef
Frankdias92 <frankmcdias@gmail.com>
2024-09-12 22:16:39 -0300

    :ok_hand: Style: building progres bar | first step

--- mobile/src/components/ui/ProgresBar.tsx
+++ mobile/src/components/ui/ProgresBar.tsx
@@ -1,12 +1,44 @@
 import { Text } from "@gluestack-ui/themed";
-import { View } from "react-native";
+import { Animated, StyleSheet, View } from "react-native";
 
 
 export function ProgresBarProgress() {
 
     return (
-        <View className="min-w-full h-2 bg-neutral-800 ounded-full">
-            <View className="min-w-full h-2  rounded-full"/>
+        <View style={styles.container} >
+            <View style={styles.progressBar} >
+                <Animated.View style={styles.fillBar} />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        minWidth: '100%',
+        width: '100%',
+        flexDirection: "column", //column direction
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        backgroundColor: '#a08',
+        padding: 8,
+        borderRadius: 50
+    },
+    progressBar: {
+        height: 20,
+        flexDirection: "row",
+        width: 'auto',
+        backgroundColor: '$secondary400',
+        borderColor: '#a04',
+        borderWidth: 2,
+        borderRadius: 50
+    },
+    fillBar: {
+        display: "flex",
+        width: '10%',
+        alignItems: "flex-end",
+        backgroundColor: '#000',
+        borderRadius: 50,
+    }
+  });
