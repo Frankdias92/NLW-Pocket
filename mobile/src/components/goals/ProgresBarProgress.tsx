cd8826ab4c173524c9c9d7c2b599ffbb5713e695
Frankdias92 <frankmcdias@gmail.com>
2024-09-12 23:11:14 -0300

    :ok_hand: Style: building progress bar ok

--- mobile/src/components/goals/ProgresBarProgress.tsx
+++ mobile/src/components/goals/ProgresBarProgress.tsx
@@ -7,19 +7,7 @@ import { View, StyleSheet } from 'react-native'
 export function ProgresBar() {
     return (
         <Center w={"$full"} gap={12} px={'$8'}>
-            {/* <View 
-                className="min-w-full h-2 bg-violet-600 rounded-full"
-                style={styles.example}
-            >
-                <ProgressBar
-                    styleAttr="Horizontal"
-                    indeterminate={false}
-                    progress={0.75}
-                />
-            </View> */}
-            <ProgresBarProgress />
-            
-            {/* <View className="min-w-full h-2 bg-violet-600 rounded-full"/> */}
+            <ProgresBarProgress progress={40} />
             <HStack w={"$full"} justifyContent="space-between">
                 <Text alignSelf="flex-start" color="$secondary400">
                     Você completou <Text color="$secondary100">8</Text> de <Text color="$secondary100">15</Text>.
--- mobile/src/components/ui/ProgresBar.tsx
+++ mobile/src/components/ui/ProgresBar.tsx
@@ -1,13 +1,29 @@
 import { Text } from "@gluestack-ui/themed";
+import { useEffect, useRef } from "react";
 import { Animated, StyleSheet, View } from "react-native";
 
 
-export function ProgresBarProgress() {
+export function ProgresBarProgress({ progress }: { progress: number }) {
+    const progressTask = useRef(new Animated.Value(0)).current
+
+    useEffect(() => {
+        Animated.timing(progressTask, {
+            toValue: progress,
+            duration: 500,
+            useNativeDriver: false
+        }).start()
+    }, [progress])
+
+    const widthInterpolated = progressTask.interpolate({
+        inputRange: [0, 100],
+        outputRange: ['0%', '100%']
+    })
+    
 
     return (
         <View style={styles.container} >
             <View style={styles.progressBar} >
-                <Animated.View style={styles.fillBar} />
+                <Animated.View style={[styles.fillBar, { width: widthInterpolated }]} />
             </View>
         </View>
     )
@@ -18,25 +34,24 @@ const styles = StyleSheet.create({
         flex: 1,
         minWidth: '100%',
         width: '100%',
-        flexDirection: "column", //column direction
+        flexDirection: "column",
         justifyContent: 'center',
         alignItems: 'flex-start',
         backgroundColor: '#a08',
-        padding: 8,
-        borderRadius: 50
+        paddingVertical: 6,
+        borderRadius: 50,
     },
     progressBar: {
-        height: 20,
+        height: 15,
         flexDirection: "row",
         width: 'auto',
         backgroundColor: '$secondary400',
         borderColor: '#a04',
         borderWidth: 2,
-        borderRadius: 50
+        borderRadius: 50,
     },
     fillBar: {
         display: "flex",
-        width: '10%',
         alignItems: "flex-end",
         backgroundColor: '#000',
         borderRadius: 50,
